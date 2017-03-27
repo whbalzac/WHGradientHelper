e06cbaf684a549a9d65d8dbb70f89063efed17fa
whbalzac <Balzac@wenhaodeMacBook-Pro-2.local>
2017-03-27 20:14:36 +0800

    change some detail

--- WHGradientHelper/WHGradientHelper.m
+++ WHGradientHelper/WHGradientHelper.m
@@ -47,6 +47,7 @@
     UIGraphicsBeginImageContextWithOptions(gradientLayer.frame.size, NO, 0);
     [gradientLayer renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *gradientImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
     
     return gradientImage;
 }
@@ -122,25 +123,7 @@
     chromatoLayer.locations = @[@(0.0f) ,@(1.0f)];
     [chromatoLayer setFrame:CGRectMake(0, 0, btnWidth, btnHeight)];
     
-    CAKeyframeAnimation *chromateAnimate = [CAKeyframeAnimation animationWithKeyPath:@"colors"];
-    
-    chromateAnimate.values = @[@[(__bridge id)UIColorFronHSB(0.63, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.75, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.73, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.85, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.83, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.95, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.88, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(1, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.98, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.1, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(1, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.12, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.1, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.22, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.2, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.32, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.3, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.42, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.4, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.52, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.5, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.62, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.6, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.72, 0.69, 0.88).CGColor],
-                               @[(__bridge id)UIColorFronHSB(0.63, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.75, 0.69, 0.88).CGColor]];
-    chromateAnimate.keyTimes = @[@0, @0.1, @0.2, @0.25, @0.35, @0.37, @0.47, @0.57, @0.67, @0.77, @0.87, @0.97, @1];
-    chromateAnimate.duration = 10;
-    chromateAnimate.removedOnCompletion = NO;
-    chromateAnimate.repeatCount = MAXFLOAT;
+    CAKeyframeAnimation *chromateAnimate = [self createGradientChromatoKeyAnimation];
     
     [chromatoLayer addAnimation:chromateAnimate forKey:@"chromateAnimate"];
     
@@ -184,6 +167,20 @@
     chromatoLayer.locations = @[@(0.0f) ,@(1.0f)];
     [chromatoLayer setFrame:parentView.frame];
     
+    CAKeyframeAnimation *chromateAnimate = [self createGradientChromatoKeyAnimation];
+    
+    [chromatoLayer addAnimation:chromateAnimate forKey:@"chromateAnimate"];
+    
+    [parentView.layer addSublayer:chromatoLayer];
+    chromatoLayer.mask = lable.layer;
+    chromatoLayer.frame = chromatoLayer.bounds;
+}
+
+
+#pragma mark - private method
+
++ (CAKeyframeAnimation *)createGradientChromatoKeyAnimation
+{
     CAKeyframeAnimation *chromateAnimate = [CAKeyframeAnimation animationWithKeyPath:@"colors"];
     
     chromateAnimate.values = @[@[(__bridge id)UIColorFronHSB(0.63, 0.69, 0.88).CGColor, (__bridge id)UIColorFronHSB(0.75, 0.69, 0.88).CGColor],
@@ -204,12 +201,7 @@
     chromateAnimate.removedOnCompletion = NO;
     chromateAnimate.repeatCount = MAXFLOAT;
     
-    [chromatoLayer addAnimation:chromateAnimate forKey:@"chromateAnimate"];
-    
-    
-    [parentView.layer addSublayer:chromatoLayer];
-    chromatoLayer.mask = lable.layer;
-    chromatoLayer.frame = chromatoLayer.bounds;
+    return chromateAnimate;
 }
 
 @end
